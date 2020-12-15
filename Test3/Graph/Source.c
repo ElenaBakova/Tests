c8cf8ec4cfa4eba8e6e445a90c33c16aa8e68a48
ElenaBakova <lenabakova72@gmail.com>
2020-12-15 12:40:50 +0300

    Amended source.c

--- Test3/Graph/Source.c
+++ Test3/Graph/Source.c
@@ -7,19 +7,13 @@ int main()
 	Graph* graph = makeGraph("input.txt");
 	printGraph(graph);
 	Queue* queue = bfs(graph, 0);
-	while (!empty(queue))
+	while (!isEmpty(queue))
 	{
 		printf("%i ", dequeue(queue) + 1);
 	}
 	printf("\n");
-	Queue* dfsQueue = dfs(graph, 0);
-	while (!empty(dfsQueue))
-	{
-		printf("%i ", dequeue(dfsQueue) + 1);
-	}
+	
 	deleteQueue(&queue);
-	deleteQueue(&dfsQueue);
 	deleteGraph(&graph);
-
 	return 0;
 }
